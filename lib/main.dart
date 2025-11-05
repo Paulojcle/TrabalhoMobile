673b4fc540e8abb3d5ac48285c8bc103fd4d6b54
Andson <167490002+andsonqueiroz@users.noreply.github.com>
2025-11-05 14:13:42 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
-void main() {
-  runApp(MeuAplicativo());
-}
+import 'splash_screen.dart';
 
-class MeuAplicativo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+void main() {
+  runApp(
+    const MaterialApp(
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.white, // cor principal
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const HomePage(),
-    );
-  }
+    ),
+  );
 }
