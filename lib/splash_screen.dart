885776f3f3274f092fce15dd0e8e4039dc7481f2
Andson <167490002+andsonqueiroz@users.noreply.github.com>
2025-11-05 13:22:12 -0300

    Add files via upload

---
+++ lib/splash_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'main_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Espera 3 segundos e vai para a HomePage
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) =>  MainScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255), // cor de fundo
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Ícone ou logo do app
+            Image.asset(
+              'assets/images/logo.jpeg',
+              width: 240,
+              height: 240,
+              fit: BoxFit.contain,
+            ),
+            SizedBox(height: 20),
+            // Nome do app
+            Text(
+              "SleepWell Hotel",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20)
+          ],
+        ),
+      ),
+    );
+  }
+}
